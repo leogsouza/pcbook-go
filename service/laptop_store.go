a122dd66697494f4e95965f80cd160fcd8ff41c2
Leonardo Souza <leogsouza@gmail.com>
2020-09-23 13:03:32 -0300

    Implement grpc stream server

--- service/laptop_store.go
+++ service/laptop_store.go
@@ -16,7 +16,10 @@ var ErrAlreadyExists = errors.New("record already exists")
 type LaptopStore interface {
 	// Save saves the laptop to the store
 	Save(laptop *pb.Laptop) error
+	// Find finds a laptop by ID
 	Find(id string) (*pb.Laptop, error)
+	// Search searches for laptops with filter, returns one by one via the found function
+	Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 // InMemoryLaptopStore stores laptop in memory
@@ -41,10 +44,9 @@ func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
 		return ErrAlreadyExists
 	}
 
-	other := &pb.Laptop{}
-	err := copier.Copy(other, laptop)
+	other, err := deepCopy(laptop)
 	if err != nil {
-		return fmt.Errorf("cannot copy laptop data: %w", err)
+		return err
 	}
 
 	store.data[other.Id] = other
@@ -61,7 +63,72 @@ func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 		return nil, nil
 	}
 
-	// deep copy
+	return deepCopy(laptop)
+}
+
+// Search searches for laptops with filter, returns one by one via the found function
+func (store *InMemoryLaptopStore) Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.GetMaxPriceUsd() {
+		return false
+	}
+
+	if laptop.GetCpu().GetNumberCores() < filter.GetMinCpuCores() {
+		return false
+	}
+
+	if laptop.GetCpu().GetMinGhz() < filter.GetMinCpuGhz() {
+		return false
+	}
+
+	if toBit(laptop.GetRam()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+
+	return true
+}
+
+func toBit(memory *pb.Memory) uint64 {
+	value := memory.GetValue()
+
+	switch memory.GetUnit() {
+	case pb.Memory_BIT:
+		return value
+	case pb.Memory_BYTE:
+		return value << 3 // 8 = 2^3
+	case pb.Memory_KILOBYTE:
+		return value << 13 // 1024 * 8 = 2^10 * 2^3 = 2^13
+	case pb.Memory_MEGABYTE:
+		return value << 23
+	case pb.Memory_GIGABYTE:
+		return value << 33
+	case pb.Memory_TERABYTE:
+		return value << 43
+	default:
+		return 0
+	}
+}
+
+func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
 	other := &pb.Laptop{}
 	err := copier.Copy(other, laptop)
 	if err != nil {
